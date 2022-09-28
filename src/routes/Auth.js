bb81f37aa15bd1e778561755df9f97177af32071
ming-sil <choming4926@gmail.com>
2022-09-28 23:32:04 +0900

    Login Form pt.1

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Auth = () => <span>Auth</span>;
+export const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
