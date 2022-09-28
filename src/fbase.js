3d5b568dcad8fe16d622cbb4a5312bc08071ea9a
ming-sil <choming4926@gmail.com>
2022-09-28 23:09:35 +0900

    Using Firebase Auth fix

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-export const firebase = initializeApp(firebaseConfig);
-export const authService = firebase.auth();
+const app = initializeApp(firebaseConfig);
+export const authService = getAuth();
