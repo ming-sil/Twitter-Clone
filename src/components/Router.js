eb02103f2ab127f2e0718608ffbc394e9ff78886
ming-sil <choming4926@gmail.com>
2022-10-04 23:33:13 +0900

    Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { Auth } from "../routes/Auth";
 import { Home } from "../routes/Home";
+import { Profile } from "../routes/Profile";
+import { Navigation } from "./Navigation";
 
 export const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </>
         ) : (
           <>
             <Route path="/" element={<Auth />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </>
         )}
       </Routes>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,13 @@
 import React from "react";
+import { authService } from "../fbase";
 
-export const Profile = () => <span>Profile</span>;
+export const Profile = () => {
+  const onLogOutClick = () => {
+    authService.signOut();
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
