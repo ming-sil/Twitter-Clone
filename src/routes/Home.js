b0f4fdf077cafb5a9b7a9efe9b5e785523781562
ming-sil <choming4926@gmail.com>
2022-10-05 23:13:11 +0900

    Form and Database Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Home = () => <span>Home</span>;
+export const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
+};
